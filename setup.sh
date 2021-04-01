1546d06a22d603aa2bc223c5283ea8d6352895a2
Denis Komarkov <44139611+KD3n1z@users.noreply.github.com>
2021-04-01 11:40:19 +0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt install dos2unix
-apt install python
-pip3 install colorama
-pip3 install requests
-cp ./stalker.py $PREFIX/bin/vk-stalker
-dos2unix $PREFIX/bin/vk-stalker
-chmod 777 $PREFIX/bin/vk-stalker
