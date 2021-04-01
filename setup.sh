dcc6856381637177677ab971e34cf3a3d3472185
Denis Komarkov <44139611+KD3n1z@users.noreply.github.com>
2021-04-01 11:18:51 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 apt install python3
 pip3 install colorama
 pip3 install requests
-chmod 777 $PREFIX/bin/vk-stalker
 cp ./stalker.py $PREFIX/bin/vk-stalker
+chmod 777 $PREFIX/bin/vk-stalker
